03833e00ab13dd214257e530fd36054a92c5b6f7
Alexandre Cougard <alexandre.cougard@gmail.com>
2014-09-26 14:45:32 +0200

    Create splash

---
+++ splash.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	 <title>Mon sport Ma Ville &amp; Moi - Nantes</title>
+	<meta name="viewport" content="width=1024" />
+    <meta name="description" content="Mon sport, ma ville et Moi - Nantes">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="apple-touch-icon" sizes="57x57" href="apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon-180x180.png">
+	<link rel="icon" type="image/png" href="favicon-192x192.png" sizes="192x192">
+	<link rel="icon" type="image/png" href="favicon-160x160.png" sizes="160x160">
+	<link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16">
+	<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32">
+	<meta name="msapplication-TileColor" content="#aacfd4">
+	<meta name="msapplication-TileImage" content="mstile-144x144.png">
+</head>
+<body>
+	<div class="wrapper__intro">
+        <div class="wrapper__intro__body">
+            <div class="wrapper__intro__body__inner">
+                <h1 class="entry-title">Mon sport Ma Ville &amp; Moi - Nantes</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat dolore aliquid, ea inventore alias dolorum, omnis officia recusandae voluptates explicabo, possimus deserunt sunt. Suscipit excepturi, nulla nemo culpa quos inventore.
+                </p>
+                <div class="loading">
+                    <a href="/" class="bt-explore">Découvrir</a>
+                </div><!-- .loading -->
+            </div>
+        </div><!-- .wrapper__intro__body -->
+    </div><!-- .wrapper__intro -->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-35099699-2', 'auto');
+		ga('send', 'pageview');
+	</script>
+</body>
+</html>
